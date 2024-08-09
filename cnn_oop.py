55caecc08df45644698a82ac71ea406db2d020bd
Jagdeep Singh <jagdeeptheawesome@gmail.com>
2024-08-09 16:16:17 +0800

    Created the Modify_input_output method, dimensions followed pytorch version

--- cnn_oop.py
+++ cnn_oop.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from typing import Union, Literal, Tuple
+import math
 
 
 class CNN:
@@ -27,7 +28,7 @@ class Sequence:
 class Conv2D:
     # Input shape should be m, 3, n1, n2
     # Kernel shape should be 3, f1, f2
-    def __init__(self, in_channels=int, out_channels=int, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, Tuple[int, int]], padding=Union[None, Literal['VALID', 'SAME']]):
+    def __init__(self, in_channels=int, out_channels=int, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, int], padding=Union[None, Literal['VALID', 'SAME']]):
         self.in_channels = in_channels
         self.out_channels = out_channels
 
@@ -40,17 +41,18 @@ class Conv2D:
             self.k2 = kernel_size[1]
             self.kernel_size = (out_channels, in_channels, self.k1, self.k2)
         
-        self.stride = stride
+        if stride == None:
+            self.stride = 1
+        else:
+            self.stride = stride
+        
         self.padding = padding
 
         self.kernel = np.random.randn(*self.kernel_size)
         self.bias = np.random.randn(*self.kernel_size) #Need to check if dimensions are correct
-
-
         return
     
-    def forward(self, X):
-        # Reshape X for padding
+    def modify_input_output(self, X):
         if self.padding == 'SAME':
 
             # Formula for calculating padding requirement for 'SAME' padding
@@ -59,37 +61,149 @@ class Conv2D:
             self.p = (self.kernel_size[2] - 1) / 2
             
             # Check number of observations
-            if X.shape.len == 4:
-                # n1 and n2 represents observations width and height
+            if (X.shape.len == 4):
+                # Set input dimensions and values
                 self.observations = X.shape[0]
-                self.n1 = X.shape[2]
-                self.n2 = X.shape[3]
+                self.obs_height = X.shape[2]
+                self.obs_width = X.shape[3]
+
+                # Set output_size
+                if self.stride == 1:
+                    self.output_size = (self.observations, self.out_channels, self.obs_height, self.obs_width)
+                else:
+                    self.output_size = (self.observations, self.out_channels, math.floor(((self.obs_height + (2 * self.p) - self.k1) / self.stride) + 1), math.floor(((self.obs_width + (2 * self.p) - self.k2) / self.stride) + 1))
+
                 # For each observation and for each RGB array
+                # Set padding for input
                 for i, observation in enumerate(range(X.shape[0])):
                     for j, out_channel in enumerate(range(self.out_channels)):
                         X[i, j] =  np.pad(X[i, j], pad_width=self.p, mode='constant', constant_values=0)
 
             elif(X.shape.len == 3):
+                # Set input dimensions and values
+                self.obs_height = X.shape[1]
+                self.obs_width = X.shape[2]
                 if X.shape[0] != 3:
                     # If it is observation by n1 by n2
                     self.observations = X.shape[0]
+                    # Set output_size
+                    if self.stride == 1:
+                        self.output_size = (self.observations, self.out_channels, self.obs_height, self.obs_width)
+                    else:
+                        self.output_size = (self.observations, self.out_channels, math.floor(((self.obs_height + (2 * self.p) - self.k1) / self.stride) + 1), math.floor(((self.obs_width + (2 * self.p) - self.k2) / self.stride) + 1))
                 else:
                     # If it is In_channels by n1 by n2
                     self.observations = 1
-                self.n1 = X.shape[1]
-                self.n2 = X.shape[2]
+                    # Set output_size
+                    if self.stride == 1:
+                        self.output_size = ( self.out_channels, self.obs_height, self.obs_width)
+                    else:
+                        self.output_size = (self.out_channels, math.floor(((self.obs_height + (2 * self.p) - self.k1) / self.stride) + 1), math.floor(((self.obs_width + (2 * self.p) - self.k2) / self.stride) + 1))
 
+
+
+                # Set padding for input
                 for i, observation in enumerate(range(X.shape[0])):
                     # Can be either Observation, Height, Width or In_channels, Height, Width
                     X[i] = np.pad(X[i], pad_width=self.p, mode='constant', constant_values=0)
 
             elif(X.shape.len == 2):
                 # Only 1 observation, and just 1 In_channel
+                # Set input dimensions and values
                 self.observations = 1
+                self.obs_height = X.shape[0]
+                self.obs_width = X.shape[1]
+
+                # Set output_size
+                if self.stride == 1:
+                    self.output_size = (self.out_channels, self.obs_height, self.obs_width)
+                else:
+                    self.output_size = (self.out_channels, math.floor(((self.obs_height + (2 * self.p) - self.k1) / self.stride) + 1), math.floor(((self.obs_width + (2 * self.p) - self.k2) / self.stride) + 1))
+
+
+                # Set padding for input
                 X = np.pad(X, pad_width=self.p, mode='constant', constant_values=0)
 
+            else:
+                return print('Error, wrong input dimensions')
+
+        elif (self.padding == 'VALID' or self.padding == None):
+            # Check number of observations
+            if (X.shape.len == 4):
+                # Set input dimensions and values
+                self.observations = X.shape[0]
+                self.obs_height = X.shape[2]
+                self.obs_width = X.shape[3]
+
+                # Set output_size
+                if self.stride == 1:
+                    self.output_size = (self.observations, self.out_channels, (self.obs_height - self.k1 + 1), (self.obs_width - self.k2 + 1))
+                else:
+                    self.output_size = (self.observations, self.out_channels, math.floor(((self.obs_height - self.k1) / self.stride) + 1), math.floor(((self.obs_width - self.k2) / self.stride) + 1))
+
+                # For each observation and for each RGB array
+                # Set padding for input
+                for i, observation in enumerate(range(X.shape[0])):
+                    for j, out_channel in enumerate(range(self.out_channels)):
+                        X[i, j] =  np.pad(X[i, j], pad_width=self.p, mode='constant', constant_values=0)
+
+            elif(X.shape.len == 3):
+                # Set input dimensions and values
+                self.obs_height = X.shape[1]
+                self.obs_width = X.shape[2]
+                if X.shape[0] != 3:
+                    # If it is observation by n1 by n2
+                    self.observations = X.shape[0]
+                    # Set output_size
+                    if self.stride == 1:
+                        self.output_size = (self.observations, self.out_channels, (self.obs_height - self.k1 + 1), (self.obs_width - self.k2 + 1))
+                    else:
+                        self.output_size = (self.observations, self.out_channel, math.floor(((self.obs_height - self.k1) / self.stride) + 1), math.floor(((self.obs_width - self.k2) / self.stride) + 1))
+                
+                else:
+                    # If it is In_channels by n1 by n2
+                    self.observations = 1
+                    # Set output_size
+                    if self.stride == 1:
+                        self.output_size = (self.out_channels, (self.obs_height - self.k1 + 1), (self.obs_width - self.k2 + 1))
+                    else:
+                        self.output_size = (self.out_channels, math.floor(((self.obs_height - self.k1) / self.stride) + 1), math.floor(((self.obs_width - self.k2) / self.stride) + 1))
+
+                # Set padding for input
+                for i, observation in enumerate(range(X.shape[0])):
+                    # Can be either Observation, Height, Width or In_channels, Height, Width
+                    X[i] = np.pad(X[i], pad_width=self.p, mode='constant', constant_values=0)
+
+            elif(X.shape.len == 2):
+                # Only 1 observation, and just 1 In_channel
+                # Set input dimensions and values
+                self.observations = 1
+                self.obs_height = X.shape[0]
+                self.obs_width = X.shape[1]
+
+                # Set output_size
+                if self.stride == 1:
+                    self.output_size = (self.out_channels, (self.obs_height - self.k1 + 1), (self.obs_width - self.k2 + 1))
+                else:
+                    self.output_size = (self.out_channels, math.floor(((self.obs_height - self.k1) / self.stride) + 1), math.floor(((self.obs_width - self.k2) / self.stride) + 1))
+
+
+                # Set padding for input
+                X = np.pad(X, pad_width=self.p, mode='constant', constant_values=0)
+
+            else:
+                return print('Error, wrong input dimensions')
+
+        else:
+            return print('Error with padding method')
 
         self.output = np.empty(self.output_size)
+        return
+        
+    def forward(self, X):
+        # Reshape X for padding and creating output_size
+
+        self.modify_input_output(X)
 
 
         # Consideration for stride as well
@@ -114,7 +228,7 @@ class ReLU:
 
 
 class MaxPool2D:
-    def __init__(self, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, int, Tuple[int, int]], padding=Union[None, int, Tuple[int, int]]):
+    def __init__(self, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, int], padding=Union[None, int, Tuple[int, int]]):
         self.kernel_size = kernel_size
         if stride != kernel_size:
             self.stride = stride
@@ -127,7 +241,7 @@ class MaxPool2D:
         return # matrix and dimensions for next Convolutional Layer
 
 class AvePool2D:
-    def __init__(self, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, int, Tuple[int, int]], padding=Union[None, int, Tuple[int, int]]):
+    def __init__(self, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, int], padding=Union[None, int, Tuple[int, int]]):
         self.kernel_size = kernel_size
         if stride != kernel_size:
             self.stride = stride
