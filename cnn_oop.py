Completed Backpropagation fully, without testing, fixed a bit of forward propagation and completed SGD and One Hot Y transformation as well

cnn_oop.py
@@ -17,12 +17,19 @@ class Sequence:
         self.layers = args
 
     def forward(self, X):
-        self.input = X
+        self.X = X
         A = X
         for layer in self.layers:
             A = layer.forward(A)
         self.output = A
         return self.output
+    
+    def get_layers(self):
+        return self.layers
+    
+    def get_X(self):
+        return self.X
+        
 
 
 class Conv2D:
@@ -56,6 +63,7 @@ class Conv2D:
     
     def modify_input_output(self, X):
         # Setting X for modification for forward propagation
+        # X dimension is expanded for consistency of ensuring everything stays 5D
         self.X = X
         if self.padding == 'SAME':
 
@@ -240,17 +248,105 @@ class Conv2D:
         # For each observation
         for i in range(self.output_size[0]):
             # For each kernel
-            for j in range(self.output_size[1]):
+            for j in range(self.out_channels):
                 # For each RGB in_channel
                 for k in range(self.in_channels):
                     # For each row operation for output
-                    for l in range(self.output_size[2]):
+                    for l in range(self.output_size[3]):
                         # For each clm operation per row operation
-                        for m in range(self.output_size[3]):
+                        for m in range(self.output_size[4]):
                             region = X[i, k, (l*self.stride):(l*self.stride+self.k1), (m*self.stride):(m*self.stride+self.k2)]
                             self.output[i, 0, j, l, m] += np.sum(region * self.kernel[j, k])
                             self.output += self.bias(j)
         return self.output
+
+    def get_kernel(self):
+        return self.kernel
+    
+    def get_bias(self):
+        return self.bias
+    
+    def find_and_set_dK(self, dZ):
+        self.dZ = dZ
+        # input dimensions are: (obs, in, none, n1, n2) or (obs, 1, out, n1, n2)
+        # dZ dimensions are: (obs, 1, out, n1, n2)
+        # dK should be: case 1: (obs, 1, out, n1, n2), case 2: (obs, 1, out, n1, n2)
+        self.dK_observations = self.dZ.shape[0]
+        self.dK_in_channels = self.dZ.shape[1]
+        self.dK_out_channels = self.dZ.shape[2]
+        self.dK_height = (self.X.shape[3] - self.dZ.shape[3]) + 1
+        self.dK_width = (self.X.shape[4] - self.dZ.shape[4]) + 1
+        self.dK_size = (self.dK_observations, self.dK_in_channels, self.dK_out_channels, self.dK_height, self.dK_width)
+        self.dK = np.empty(*self.dK_size)
+
+        dZ_K1 = dZ.shape[3]
+        dZ_K2 = dZ.shape[4]
+
+        # Case 1: (Input)
+        if (len(self.X.shape) != 5):
+            for i in range(self.dK_observations):
+                for j in range(self.dK_out_channels):
+                    for k in range(self.dK_in_channels):
+                        for l in range(self.dK_height):
+                            for m in range(self.dK_width):
+                                region = self.X[i, k, (l):(l+dZ_K1), (m):(m+dZ_K2)]
+                                self.dK[i, 0, j, l, m] += np.sum(region * dZ[i, j, k])
+        
+        # Case 2 (From previous Activation / Pooling Function)
+        else:
+            for i in range(self.dK_observations):
+                for j in range(self.dK_out_channels):
+                    for k in range(self.dK_height):
+                        for l in range(self.dK_width):
+                            region = self.X[i, 0, j, (k):(k+dZ_K1), (l):(l+dZ_K2)]
+                            self.dK[i, 0, j, l, m] += np.sum(region * dZ[i, j, k])
+        
+        return
+
+    def find_and_set_dI(self):
+        padding = ((0, 0), (0, 0), (0, 0), (1, 1), (1, 1))
+        dZ_padded = self.dZ
+        dZ_padded = np.pad(dZ_padded, pad_width=padding, mode='constant', constant_values=0)
+        self.dI_observations = self.dZ.shape[0]
+        self.dI_in_channels = self.dZ.shape[1]
+        self.dI_out_channels = self.dZ.shape[2]
+        self.dI_height = self.dZ.shape[3] + 2 - self.k1 + 1
+        self.dI_width = self.dZ.shape[4] + 2 - self.k2 + 1
+        self.dI_size = (self.dI_observations, self.dI_in_channels, self.dI_out_channels, self.dI_height, self.dI_width)
+        self.dI = np.empty(*self.dI_size)
+
+        kernel_rotated = self.kernel
+        kernel_rotated = np.rot90(kernel_rotated, k=2)
+
+        for i in range(self.dI_observations):
+            for j in range(self.dI_out_channels):
+                for k in range(self.dI_height):
+                    for l in range(self.dI_width):
+                        region = dZ_padded[i, 0, j, (k):(k+self.k1), (l):(l+self.k2)]
+                        self.dI[i, 0, j, k, l] += np.sum(region * kernel_rotated[j, 0])
+        return
+
+    def get_dK(self):
+        return self.dK
+    
+    def get_dB(self):
+        return self.dB
+
+    def set_dB(self, dB):
+        self.dB = dB
+        return
+
+    def set_dZ(self, dZ):
+        self.dZ = dZ
+        return
+    
+    def get_dZ(self):
+        return self.dZ
+
+    def SGD_step_kernel_bias(self, alpha):
+        self.kernel = self.kernel - (alpha * self.dK)
+        self.bias = self.bias - (alpha * self.dB)
+        return
 
 class ReLU:
     def forward(self, X):
@@ -282,6 +378,7 @@ class MaxPool2D:
 
     def forward(self, X):
         # Setting observation dimensions
+        self.X = X
         self.observations = X.shape[0]
         self.in_channels = X.shape[1]
         self.out_channels = X.shape[2]
@@ -294,15 +391,34 @@ class MaxPool2D:
         self.output_size = (self.observations, self.in_channels, self.out_channels, self.output_height, self.output_width)
         self.output = np.empty(*self.output_size)
 
-        for i in range(self.observations):
-            for j in range(self.in_channels):
-                for k in range(self.out_channels):
-                    for l in range(self.obs_height):
-                        for m in range(self.obs_width):
+        for i in range(self.output_size[0]):
+            for j in range(self.output_size[1]):
+                for k in range(self.output_size[2]):
+                    for l in range(self.output_size[3]):
+                        for m in range(self.output_size[4]):
                             range = X[i, j, k, (l*self.stride):(l*self.stride+self.k1), (m*self.stride):(m*self.stride+self.k2)]
                             self.output[i, j, k, l, m] += np.max(range)
 
         return self.output
+
+    def find_and_set_dP(self, dZ):
+        self.dP = np.zeros(*self.X.shape)
+        for i in range(self.output_size[0]):
+            for j in range(self.output_size[1]):
+                for k in range(self.output_size[2]):
+                    for l in range(self.output_size[3]):
+                        for m in range(self.output_size[4]):
+                            range = self.X[i, j, k, (l*self.stride):(l*self.stride+self.k1), (m*self.stride):(m*self.stride+self.k2)]
+                            max_position = np.argmax(range)
+                            self.dP[max_position] += dZ[i, j, k, l, m]
+        return self.dP
+
+    def set_dP(self, dP):
+        self.dP = dP
+        return
+    
+    def get_dP(self):
+        return self.dP
 
 class AvePool2D:
     def __init__(self, kernel_size=Union[int, Tuple[int, int]], stride=Union[None, int]):
@@ -322,6 +438,7 @@ class AvePool2D:
 
     def forward(self, X):
         # Setting observation dimensions
+        self.X = X
         self.observations = X.shape[0]
         self.in_channels = X.shape[1]
         self.out_channels = X.shape[2]
@@ -334,18 +451,33 @@ class AvePool2D:
         self.output_size = (self.observations, self.in_channels, self.out_channels, self.output_height, self.output_width)
         self.output = np.empty(*self.output_size)
 
-        for i in range(self.observations):
-            for j in range(self.in_channels):
-                for k in range(self.out_channels):
-                    for l in range(self.obs_height):
-                        for m in range(self.obs_width):
+        for i in range(self.output_size[0]):
+            for j in range(self.output_size[1]):
+                for k in range(self.output_size[2]):
+                    for l in range(self.output_size[3]):
+                        for m in range(self.output_size[4]):
                             range = X[i, j, k, (l*self.stride):(l*self.stride+self.k1), (m*self.stride):(m*self.stride+self.k2)]
                             self.output[i, j, k, l, m] += np.mean(range)
 
         return self.output
 
-
+    def find_and_set_dP(self, dZ):
+        self.dP = np.zeros(*self.X.shape)
+        for i in range(self.output_size[0]):
+            for j in range(self.output_size[1]):
+                for k in range(self.output_size[2]):
+                    for l in range(self.output_size[3]):
+                        for m in range(self.output_size[4]):
+                            ave_amount = dZ[i, j, k, l, m] / (self.output_size[3] * self.output_size[4])
+                            range = self.dP[i, j, k, (l*self.stride):(l*self.stride+self.k1), (m*self.stride):(m*self.stride+self.k2)]
+                            range[:] = ave_amount
+        return self.dP
+    
+    def get_dP(self):
+        return self.dP
+    
 class flatten:
+
     def forward(self, X):
         # Setting X dimension values
         self.X = X
@@ -363,25 +495,142 @@ class flatten:
 
         return self.output
     
-    # To revert back to original X input, essentially "get" it back
-    def dF(self):
-        return self.X
+    def get_dZ(self):
+        return self.dZ
+    
+    def set_dZ(self, dZ):
+        self.dZ = dZ
+        return
+    
+    def get_shape(self):
+        return self.X.shape
 
 class Categorical_Cross_Entropy_CNN:
-    def __init__(self):
-        return
-    
-    def backward(self):
-        return
+    def __init__(self, output=None, labels=None):
+        self.output = output
+        self.labels = labels
 
+    def backward(self, sequence):
+        self.sequence = sequence
+        self.layers = self.sequence.get_layers()
+        # Layers are now output to input
+        self.layers = self.layers[::-1]
+        X = sequence.get_X()
+        n, m = self.labels.shape
+
+        for index, layer in enumerate(self.layers):
+
+            # Fully Connected Layers Backpropagation onwards:::
+            if isinstance(layer, Linear):
+                # Last Fully Connected layer (Output side)
+                if index == 1:
+                    A = self.layers[index+1].get_values()
+
+                    dZi = self.output - self.labels
+                    dWi = (1/m) * dZi.dot(A.T)
+                    dBi = (1/m) * np.sum(dZi, axis=1, keepdims=True)
+
+                    layer.set_dZ(dZi)
+                    layer.set_dW(dWi)
+                    layer.set_dB(dBi)
+
+                # First Fully Connected layer
+                elif isinstance(self.layers[index+1], flatten):
+                    previous_layer_weight = self.layers[index-2].get_weight()
+                    previous_layer_dZ = self.layers[index-2].get_dZ()
+                    Z = layer.get_Z()
+                    activation_derivitive = self.layers[index - 1].get_derivitive(Z)
+
+                    dZi = previous_layer_weight.T.dot(previous_layer_dZ) * activation_derivitive
+                    dWi = (1/m) * dZi.dot(X.T)
+                    dBi = (1/m) * np.sum(dZi, axis=1, keepdims=True)
+                    
+                    layer.set_dZ(dZi)
+                    layer.set_dW(dWi)
+                    layer.set_dB(dBi)
+                    
+
+                # Middile Fully Connected Layers
+                else:
+                    previous_layer_weight = self.layers[index-2].get_weight()
+                    previous_layer_dZ = self.layers[index-2].get_dZ()
+                    Z = layer.get_Z()
+                    activation_derivitive = self.layers[index - 1].get_derivitive(Z)
+                    A = self.layers[index+1].get_values()
+
+                    dZi = previous_layer_weight.T.dot(previous_layer_dZ) * activation_derivitive
+                    dWi = (1/m) * dZi.dot(A.T)
+                    dBi = (1/m) * np.sum(dZi, axis=1, keepdims=True)
+
+                    layer.set_dZ(dZi)
+                    layer.set_dW(dWi)
+                    layer.set_dB(dBi)
+
+
+            # CNN Backpropagation onwards:::
+            # derivitive of flatten method
+            elif(isinstance(layer, flatten)):
+                dZ = self.layers[index-1].get_weight().T * self.layers[index-1].get_dZ()
+                dZ = dZ.reshape(layer.get_shape())
+                layer.set_dZ(dZ)
+
+            # If current index is a Pooling layer
+            elif(isinstance(layer, MaxPool2D) or isinstance(layer, AvePool2D)):
+                dZ = self.layers[index-1].get_dZ()
+                dP = layer.find_and_set_dP(dZ)
+
+            # Setting the derivitive of ReLU activation layer
+            elif(isinstance(layer, Conv2D)):
+                if isinstance(self.layers[index-2], MaxPool2D) or isinstance(self.labels[index-2], AvePool2D):
+                    dP = self.layer[index-2].get_dP()
+                else:
+                    dP = self.layer[index-1].get_dZ()
+                dF = self.layers[index-1].get_derivitives()
+                dZ = dP.dot(dF)
+
+                dB = np.sum(dZ)
+
+                layer.find_and_set_dK(dZ)
+                layer.set_dZ(dZ)
+                layer.set_dB(dB)
+
+                # If it is not first Convolutional Layer, find dI
+                if index != (len(self.layers) - 1):
+                    layer.find_and_set_dI()
+
+
+    # Currently Labels is designed for One_Hot_Y
+    # One_Hot_Y to be n by m
     def cost(self):
-        return
+        output_clipped = np.clip(self.output, 1e-7, 1 - 1e-7)
+        predicted_values = np.sum(output_clipped*self.labels, axis=0)
+        self.cost_amount = np.mean(-np.log(predicted_values))
+        return self.cost_amount
     
+
+class One_Hot_Y:
+    # Y should be 1 by m
+    # Objective is to turn Y into n by m
+    def __init__(self, Y):
+        # Convert Y into n by m of zeros
+        One_Hot_Values = np.zeros(Y.max(), Y.size)
+        # Converts values to 1 at given rows and clmns
+        One_Hot_Values[Y, np.arange(Y.size)] = 1
+    
+    def get_one_hot_Y(self):
+        return self.Y
+
 
 class Optimizer_CNN:
     def SGD(self, sequence, alpha):
-        return
+        self.sequence = sequence
+        self.alpha = alpha
     
     def step(self):
+        for layer in self.layers:
+            if isinstance(layer, Conv2D):
+                layer.SGD_step_kernel_bias(self.alpha)
+            if isinstance(layer, Linear):
+                layer.SGD_step_weights_bias(self.alpha)
         return
     

nn_oop.py
@@ -201,7 +201,7 @@ class Categorical_Cross_Entropy:
     # One_Hot_Y to be n by m
     def cost(self):
         output_clipped = np.clip(self.output, 1e-7, 1 - 1e-7)
-        predicted_values = np.sum(output_clipped*self.labels, axis=1)
+        predicted_values = np.sum(output_clipped*self.labels, axis=0)
         self.cost_amount = np.mean(-np.log(predicted_values))
         return self.cost_amount
 
@@ -448,7 +448,7 @@ if __name__ == '__main__':
     
 
     # Training / testing / plotting / saving model
-    no_iterations = 100
+    no_iterations = 500
     cost_amounts = np.zeros(no_iterations)
     accuracy_amounts = np.zeros(no_iterations)
     for i in range(no_iterations):
@@ -456,7 +456,7 @@ if __name__ == '__main__':
         ave_cost = Categorical_Cross_Entropy(output, labels)
         ave_cost.backward(sequence)
         optimise = Optimizer()
-        optimise.SGD(sequence, 0.04)
+        optimise.SGD(sequence, 0.7)
         optimise.step()
         
         prediction = model.predict(X_train)